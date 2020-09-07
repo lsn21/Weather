0b6872c11aab9358fa85efdcf1578b7ea5f53e66
Siarhei <lsn21@ya.ru>
2020-09-07 11:44:13 +0300

    Initial Commit

---
+++ Weather/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Weather
+//
+//  Created by Siarhei on 07.09.2020.
+//  Copyright © 2020 Siarhei. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Weather/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Weather
+//
+//  Created by Siarhei on 07.09.2020.
+//  Copyright © 2020 Siarhei. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
